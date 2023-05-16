ddb85e428546d37f9359170ea26420aa3ec4638e
duarte-wallisson <wallisson.elduarte@gmail.com>
2023-05-16 11:22:23 -0300

    feat: Hello, World!

---
+++ 00-spring-initializr/src/main/java/br/com/duarte/Greeting.java
@@ -0,0 +1,13 @@
+package br.com.duarte;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Greeting {
+    private final long id;
+    private final String content;
+}
