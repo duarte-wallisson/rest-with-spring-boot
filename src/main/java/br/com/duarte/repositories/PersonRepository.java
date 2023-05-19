430ddbda5a77d575dfa5f0a3e760c33b6b332342
duarte-wallisson <wallisson.elduarte@gmail.com>
2023-05-19 18:24:30 -0300

    refactor: retirando a annotation @Repository, devido a versão do spring boot

--- src/main/java/br/com/duarte/repositories/PersonRepository.java
+++ src/main/java/br/com/duarte/repositories/PersonRepository.java
@@ -2,8 +2,6 @@ package br.com.duarte.repositories;
 
 import br.com.duarte.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 }
