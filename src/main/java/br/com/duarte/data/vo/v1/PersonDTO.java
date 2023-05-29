cc730ce1f49647e4693955df64cc6d7cce8c4e6b
duarte-wallisson <wallisson.elduarte@gmail.com>
2023-05-29 16:12:07 -0300

    refactor: working with custom serialization

--- src/main/java/br/com/duarte/data/vo/v1/PersonDTO.java
+++ src/main/java/br/com/duarte/data/vo/v1/PersonDTO.java
@@ -1,19 +1,30 @@
 package br.com.duarte.data.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 
 import java.io.Serial;
 import java.io.Serializable;
 
+@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"})
 @Data
 public class PersonDTO implements Serializable {
     @Serial
     private static final long serialVersionUID = -6491131592662625774L;
 
     private Long id;
+
+    @JsonProperty("first_name")
     private String firstName;
+
+    @JsonProperty("last_name")
     private String lastName;
+
+    @JsonIgnore
     private String gender;
+
     private String address;
 
 }
